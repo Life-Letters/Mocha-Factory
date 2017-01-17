66249d99f611a3aa9e5ad4e56db9df0615952eb1
Patrick Lai <patrick.lai@lifeletters.com.au>
2017-01-17 14:14:55 +1100

    Removed unused config

--- sample.js
+++ sample.js
@@ -6,8 +6,6 @@ var MochaFactory = require('./lib/mocha-factory.js');
 var testList = MochaFactory.convertHtmlFileToJsFile('./selenium','./selenium/export').map((f) => require(f));
 
 MochaFactory.runIDEtests({
-  test_scripts_dir: './selenium',
-  exports_dir: './selenium/export',
   selenium_server_remote: process.env.SELENIUM_SERVER_REMOTE,
   browser_name: 'chrome'
 },
